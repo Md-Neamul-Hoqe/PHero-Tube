f4ee3bf1b7f19ccfbd4f597b81df42c50e35db75
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-09-01 12:22:32 +0600

    Poseted Date added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -1,37 +1,26 @@
-/* Links */
-// https://openapi.programming-hero.com/api/videos/categories
-// https://openapi.programming-hero.com/api/videos/category/${id}
-// https://openapi.programming-hero.com/api/videos/category/1000
-
 /* Questions */
 // Discuss the scope of var, let, and const
 // Tell us the use cases of null and undefined
 // What do you mean by REST API?
 
 /* To load categories */
+const loadingToggle = document.getElementById("loader");
 const loadCategories = async () => {
+  loadingToggle.classList.remove("hidden");
+
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
   const data = await res.json();
 
+  /* show the category buttons */
   showButtons(data.data);
+
+  /* load the videos */
+  loadData();
 };
-loadCategories();
 
-/* To load data */
-const loadData = async (id = 1000) => {
-  console.log(`https://openapi.programming-hero.com/api/videos/category/${id}`);
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/${id}`
-  );
-  const data = await res.json();
-
-  //   showData(data.data);
-  sortByViews(data.data);
-};
-loadData();
-
+/* set categories as buttons */
 const showButtons = (data) => {
   const btnContainer = document.getElementById("buttons");
 
@@ -39,13 +28,38 @@ const showButtons = (data) => {
     const btn = document.createElement("button");
     btn.setAttribute(
       "class",
-      "btn bg-gray-600/20 font-medium active:bg-red-button active:text-white active:font-semibold hover:bg-red-button hover:text-white hover:font-semibold"
+      "btn bg-gray-600/15 text-gray-600 font-medium active:bg-red-button active:text-white active:font-semibold hover:bg-red-button hover:text-white hover:font-semibold"
     );
     btn.setAttribute("onclick", `loadData('${cat.category_id}')`);
     btn.innerText = cat.category;
 
     btnContainer.appendChild(btn);
   });
+};
+
+/* To load data */
+const loadData = async (id = 1000, isSorted = false) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const data = await res.json();
+  const videos = data.data;
+
+  /* sort the videos as views on btn clicked */
+  if (isSorted) {
+    videos.sort((a, b) => {
+      const first = b?.others?.views.slice(0, -1);
+      const second = a?.others?.views.slice(0, -1);
+
+      const FE = parseFloat(first);
+      const SE = parseFloat(second);
+
+      return FE * 1000 - SE * 1000;
+    });
+  }
+
+  /* Show the fetched data */
+  showData(videos);
 };
 
 const showData = (videos) => {
@@ -68,17 +82,32 @@ const showData = (videos) => {
       cardBody.setAttribute("class", "card-body px-0 py-5");
       verifiedName.setAttribute("class", "py-1 flex items-center");
 
-      videoDescription.innerHTML = `<h1 class="leading-[26px] font-bold">${video.title}</h1>
+      card.innerHTML = `<figure class='rounded-lg relative'><img class='rounded-lg md:min-w-[312px] md:h-[20vw]' src="${video?.thumbnail}" alt="thumbnail image" /></figure>`;
+
+      /* posted date */
+      if (video?.others?.posted_date) {
+        const [H, M] = secondeToTime(video?.others?.posted_date);
+        const postedDateStatus = document.createElement("div");
+        postedDateStatus.setAttribute(
+          "class",
+          "bg-black text-white text-[10px] absolute right-3 bottom-3 py-1 px-[5px] rounded"
+        );
+        postedDateStatus.innerHTML = `<span>${H}</span>hrs <span>${M} </span>min ago`;
+        card.children[0].appendChild(postedDateStatus);
+        console.log(H, M);
+      }
+
+      videoDescription.innerHTML = `<h1 class="leading-[26px] text-header font-bold">${video.title}</h1>
     <p class="py-1"><span class='views'>${video?.others?.views}</span> views</p>`;
+
       cardBody.innerHTML = `<div class="flex items-start gap-3"></div>`;
-      cardBody.children[0].innerHTML = `<div class="avatar">
-    <div class="w-10 rounded-full">
-        <img src="${video?.authors[0]?.profile_picture}" />
-    </div>
-</div>`;
 
-      card.innerHTML = `<figure class='rounded-lg'><img class='rounded-lg md:min-w-[312px] md:h-[20vw]' src="${video?.thumbnail}" alt="thumbnail image" /></figure>`;
+      cardBody.children[0].innerHTML = `
+        <div class="avatar"><div class="w-10 rounded-full">
+            <img src="${video?.authors[0]?.profile_picture}" />
+        </div>`;
 
+      /* Verification status */
       if (video?.authors[0]?.verified) {
         verifiedName.innerHTML = `${video?.authors[0]?.profile_name} <img class='inline pl-5' src="./assets/tick.svg" alt="verified">`;
       } else {
@@ -88,6 +117,9 @@ const showData = (videos) => {
 
       cardBody.children[0].appendChild(videoDescription);
       card.appendChild(cardBody);
+
+      /* toggle loading */
+      loadingToggle.classList.add("hidden");
       videoContainer.appendChild(card);
     });
   } else {
@@ -105,25 +137,21 @@ const showData = (videos) => {
   }
 };
 
-function sortByViews(videos) {
-  console.log(videos);
-  videos.sort(sortByView);
-  console.log(videos);
-  videos.forEach((video) => {
-    console.log(video?.others?.views);
-  });
+loadCategories();
 
-  function sortByView(a, b) {
-    const first = a?.others?.views;
-    const second = b?.others?.views;
-    const aL = a?.others?.views.length - 2;
-    const bL = b?.others?.views.length - 2;
-    console.log(aL, first[aL + 1], v = first.slice(aL, 1), v);
-    console.log(
-      a?.others?.views.slice(aL, 1),
-      b?.others?.views.slice(bL, 1),
-      a?.others?.views.slice(aL, 1) - b?.others?.views.slice(bL, 1)
-    );
-    return a?.others?.views - b?.others?.views;
-  }
-}
+const sorter = (isSorted) => {
+  loadData(undefined, isSorted);
+};
+
+const secondeToTime = (second) => {
+  const s = parseInt(second);
+
+  console.log(typeof s, s);
+
+  const H = Math.floor(s / 3600);
+  const M = Math.floor((s % 3600) / 60);
+  return [H, M];
+};
+
+// console.log(Math.floor((14200 % 3600) / 60));
+// Converted time: 3hrs 56min ago
